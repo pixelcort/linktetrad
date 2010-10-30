0f30603325abbfbca94b9f61974e67494adbf020
Cortland Klein <me@pixelcort.com>
2010-10-29 22:46:14 -0700

    New methods work in progress on Cell.

--- js/cell.js
+++ js/cell.js
@@ -12,13 +12,35 @@ LT.Cell.prototype = {
     context.closePath();
     context.fillStyle = '#fff';
     context.fill();
-  }
+  },
+  dropPiece: function(piece) {
+    // If this cell already has a piece, return the piece was not dropped
+    if (this.piece) return false;
+
+    // If there is a cell below us and it does not have a piece, dropPiece on it instead
+    if (this.neighbor('down') && (!this.neighbor('down').piece)) return this.lowerCell().dropPiece(piece);
+
+    // Drop the piece here
+    this.piece = piece;
+    
+    // TODO: Tell the piece to draw itself here
+
+    return true; // Piece was dropped
+  },
+  neighbor: function(direction) { // Returns the neighbor cell, or null if it is out of bounds
+    // Check if the direction is out of bounds
+  },
+  x:null,
+  y:null
 };
 
 for (var i=0,l=LT.board.rows;i<l;i++) {
   for (var j=0,m=LT.board.columns;j<m;j++) {
     if (!LT.board.cells[i]) LT.board.cells[i] = [];
 
-    LT.board.cells[i][j] = new LT.Cell();
+    var newCell = new LT.Cell();
+    newCell.x = j;
+    newCell.y = i;
+    LT.board.cells[i][j] = newCell;
   }
 }
