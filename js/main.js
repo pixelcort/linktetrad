b7f653474d345b9ad0504de2ffe80ca0a95fb03a
Cortland Klein <me@pixelcort.com>
2010-10-29 22:49:36 -0700

    TODO: Check the board to see if the currentPlayer won.

--- js/main.js
+++ js/main.js
@@ -30,6 +30,8 @@ LT.main = function() {
     // If the pieceDropped, Toggle the currentPlayer
     if (pieceDropped) {
       LT.board.currentPlayer = (LT.board.currentPlayer === LT.Player.red) ? LT.Player.black : LT.Player.red;
+
+      // TODO: Check the board to see if the currentPlayer won. We'll only need to check the pieces nearby the newly dropped piece
     }
   });
 };
