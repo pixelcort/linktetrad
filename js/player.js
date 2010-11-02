c98e94580c1ad82b5be169c0aa471e229f8ae313
Cortland Klein <me@pixelcort.com>
2010-11-01 21:56:45 -0700

    The names of the two teams.

--- js/player.js
+++ js/player.js
@@ -8,8 +8,10 @@ LT.Player.prototype = {
 
 LT.Player.red = new LT.Player();
 LT.Player.red.team = 'red';
+LT.Player.red.name = "Player 1";
 
 LT.Player.black = new LT.Player();
 LT.Player.black.team = 'black';
+LT.Player.black.name = "Player 2";
 
 LT.board.currentPlayer = LT.Player.red; // According to http://en.wikipedia.org/wiki/Connect_Four#Attack_or_defend.3F
